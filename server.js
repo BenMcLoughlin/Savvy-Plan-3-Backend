ddc31b5a5283b748bcf0ed7386857fe7c2b7bfc1
Ben McLoughlin <benmcl@shaw.ca>
2020-12-10 09:47:29 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+//stripe
+//const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: './config.env' });
+}
+
+const app = require('./app');
+
+const DB = process.env.DATABASE_COMPASS.replace(
+  '<username>',
+  process.env.DB_USERNAME
+)
+  .replace('<password>', process.env.DB_PASSWORD)
+  .replace('<dbname>', process.env.DB_NAME);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
